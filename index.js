c247aceb7df2a869c1b914f4b691d16a07f0c109
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-04-10 19:42:20 +0200

    My first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 750 &&  
+      $(".navbar-inverse").addClass("opacity","none")){
+      // $(".navbar-inverse").fadeIn('6000')
+    }
+
+    else if($(window).scrollTop() <= 750 ){
+      $(".navbar-inverse").removeClass("opacity", "initial")
+    }
+  })
+})
+
+
+
+// $(document).ready(function () {
+//     $(".headerOne").hide();
+//     $(".headerOne").slideDown(1000);
+//     $(".headerOne").hide();
+//     $(".headerOne").fadeIn(2000);
+    // $(".headerOne h1").hide();
+    // $(".headerOne h1").fadeIn(3000);
+  
+   
+// });
+
