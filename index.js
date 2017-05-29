bd17180899e14505414e4904bcb617d1096b9991
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-05-29 23:07:48 +0200

    Set up the page

--- index.js
+++ index.js
@@ -13,58 +13,83 @@ $(document).ready(function(){
   })
 })
 
-//text slide in, in jQuery
 
-// $(document).ready(function () {
-//     $(".headerOne").hide();
-//     $(".headerOne").slideDown(1000);
-//     $(".headerOne").hide();
-//     $(".headerOne").fadeIn(2000);
-    // $(".headerOne h1").hide();
-    // $(".headerOne h1").fadeIn(3000);
-  
-   
-// });
+//Modal
+    $(document).ready(function () {
+      $(".activate-modal").click(function (e) {
+        e.preventDefault();
+        $(".modal-wrapper").fadeIn();
+      });
+      $(".exit").click(function (e) {
+        e.preventDefault();
+        $(".modal-wrapper").fadeOut();
+      });
+       $(".exit").click(function (e) {
+        e.preventDefault();
+        $(".modal-wrapper").fadeOut();
+      });
+    });
 
+
+//Typewriter
  document.addEventListener('DOMContentLoaded',function(event){
-  // array with texts to type in typewriter
-  var dataText = [ "I am Franka", "A Journalist", "From Amsterdam", "Who loves to code"];
-  
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
-  function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
-    if (i < (text.length)) {
-      // add next character to h1
-     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+  var dataText = [ "   ", "I am Franka", "A Journalist", "From Amsterdam", "Who loves to code"];
 
-      // wait for a while and call this function again for next character
+  function typeWriter(text, i, fnCallback) {
+    if (i < (text.length)) {
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
       setTimeout(function() {
         typeWriter(text, i + 1, fnCallback)
       }, 100);
     }
-    // text finished, call callback if there is a callback function
     else if (typeof fnCallback == 'function') {
-      // call callback after timeout
-      setTimeout(fnCallback, 1500);
+      setTimeout(fnCallback, 700);
     }
   }
-  // start a typewriter animation for a text in the dataText array
    function StartTextAnimation(i) {
      if (typeof dataText[i] == 'undefined'){
         setTimeout(function() {
           StartTextAnimation(0);
         }, 30000);
      }
-     // check if dataText[i] exists
     if (i < dataText[i].length) {
-      // text exists! start typewriter animation
      typeWriter(dataText[i], 0, function(){
-       // after callback (and whole text has been animated), start next text
        StartTextAnimation(i + 1);
      });
     }
   }
-  // start the text animation
   StartTextAnimation(0);
 });
+      
+// CounterUp the states
+  $(document).ready(function( $ ) {
+    if($("span.count").length > 0){ 
+      $('span.count').counterUp({
+          delay: 10, // the delay time in ms
+      time: 1500 // the speed time in ms
+      });
+    }
+  });
+
+//Carousel
+
+$(document).ready(function () {
+    var index = 0;
+    var slides = $('.slide');
+    var numImages = slides.length;
+
+    $(".slide").hide();
+    $("#slideOne").show();
+
+    function carousel() {
+        $(slides[index]).hide();
+        index = (index + 1) % numImages;
+        $(slides[index]).show();
+    }
+
+    timer = setInterval(carousel, 5000);
+});
+
+
+
+  
